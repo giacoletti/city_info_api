accc1c1a14ff3ab3128a70d29c324080a0f53235
Giovanni Iacoletti <giovanni.iacoletti@addiva.se>
2023-07-26 14:15:02 +0200

    Add CityInfoContext, install EntityFrameworkCore for SQLite, install EntityFrameworkCore.Tools, setup and initialize SQLite db with Cities and PointsOfInterest tables

---
+++ CityInfo.API/DbContexts/CityInfoContext.cs
@@ -0,0 +1,21 @@
+﻿using CityInfo.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CityInfo.API.DbContexts
+{
+    public class CityInfoContext : DbContext
+    {
+        public DbSet<City> Cities { get; set; } = null!;
+        public DbSet<PointOfInterest> PointsOfInterest { get; set; } = null!;
+
+        public CityInfoContext(DbContextOptions<CityInfoContext> options) : base(options) 
+        { 
+        }
+
+        /*protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("connection_string_here");
+            base.OnConfiguring(optionsBuilder);
+        }*/
+    }
+}
