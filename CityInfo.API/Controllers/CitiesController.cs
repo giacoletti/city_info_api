15f6ae2f974053d12c6602943deb53a8fd3a53a5
Giovanni Iacoletti <giovanni.iacoletti@addiva.se>
2023-07-27 11:23:31 +0200

    Add CityWithoutPointsOfInterestDto, replace citiesDataStore with cityInfoRepository in CitiesController, update GetCities action to fetch from database, disable GetCity action

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -1,4 +1,5 @@
 ﻿using CityInfo.API.Models;
+using CityInfo.API.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CityInfo.API.Controllers
@@ -7,22 +8,35 @@ namespace CityInfo.API.Controllers
     [Route("api/cities")]
     public class CitiesController : ControllerBase
     {
-        private readonly CitiesDataStore _citiesDataStore;
+        private readonly ICityInfoRepository _cityInfoRepository;
 
-        public CitiesController (CitiesDataStore citiesDataStore)
+        public CitiesController (ICityInfoRepository cityInfoRepository)
         {
-            _citiesDataStore = citiesDataStore ?? throw new ArgumentNullException(nameof(citiesDataStore));
+            _cityInfoRepository = cityInfoRepository ?? throw new ArgumentNullException(nameof(cityInfoRepository));
         }
 
         [HttpGet]
-        public ActionResult<IEnumerable<CityDto>> GetCities()
+        public async Task<ActionResult<IEnumerable<CityWithoutPointsOfInterestDto>>> GetCities()
         {
-            return Ok(_citiesDataStore.Cities);
+            var cityEntities = await _cityInfoRepository.GetCitiesAsync();
+
+            var results = new List<CityWithoutPointsOfInterestDto>();
+            foreach (var cityEntity in cityEntities)
+            {
+                results.Add(new CityWithoutPointsOfInterestDto
+                {
+                    Id = cityEntity.Id,
+                    Name = cityEntity.Name,
+                    Description = cityEntity.Description
+                });
+            }
+
+            return Ok(results);
         }
         [HttpGet("{id}")]
         public ActionResult<CityDto> GetCity(int id) 
         {
-            // find city
+            /*// find city
             var city = _citiesDataStore.Cities.FirstOrDefault(c => c.Id == id);
             
             if (city is null) 
@@ -30,7 +44,9 @@ namespace CityInfo.API.Controllers
                 return NotFound();
             }
 
-            return Ok(city);
+            return Ok(city);*/
+
+            return Ok();
         }
     }
 }
