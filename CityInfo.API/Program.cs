f209e970d29a27970a62ce685b3ff9d22bfbb97b
Giovanni Iacoletti <giovanni.iacoletti@addiva.se>
2023-07-31 15:59:31 +0200

    Add custom policy: User.City must be equal to 'Antwerp', apply policy to PointsOfInterestController

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -64,6 +64,16 @@ builder.Services.AddAuthentication("Bearer")
         }
      );
 
+// custom Authorization policy
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("MustBeFromAntwerp", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+        policy.RequireClaim("city", "Antwerp");
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
